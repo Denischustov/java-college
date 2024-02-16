ce3f3ec4bc780d92685df67cdb32de4797c48b2d
Denis Shustov <denishustov@icloud.com>
2024-02-16 13:49:13 +0300

    modify climber project

--- src/climber/Climber.java
+++ src/climber/Climber.java
@@ -6,16 +6,18 @@ public class Climber {
         Mountain mountain2 = new Mountain("K2", "Пакистан", 8611);
         Mountain mountain3 = new Mountain("Эверест", "Непал", 8848);
 
-        ClimbingGroup group1 = new ClimbingGroup(false, 3, mountain1);
+        ClimbingGroup group1 = new ClimbingGroup(true, 3, mountain1);
         group1.addAlpinist(new Alpinist("Иван", 30, "Москва"));
         group1.addAlpinist(new Alpinist("Петр", 27, "Санкт-Петербург"));
         group1.addAlpinist(new Alpinist("Саша", 20, "Санкт-Петербург"));
-
 
         ClimbingGroup group2 = new ClimbingGroup(true, 3, mountain2);
         group2.addAlpinist(new Alpinist("Денис", 28, "Москва"));
 
         ClimbingGroup group3 = new ClimbingGroup(true, 3, mountain3);
         group3.addAlpinist(new Alpinist("Олег", 24, "Санкт-Петербург"));
+
+
+        group1.getAlpinist();
     }
 }
--- src/climber/ClimbingGroup.java
+++ src/climber/ClimbingGroup.java
@@ -5,22 +5,32 @@ import java.util.ArrayList;
 public class ClimbingGroup {
     public boolean isRecruitment;
     public int numOfAlpinists;
-    public ArrayList<Alpinist> alpinists;
+    public Alpinist[] alpinists;
     public Mountain mountain;
+    public int i = 0;
 
     public ClimbingGroup(boolean isRecruitment, int numOfAlpinists, Mountain mountain) {
         this.isRecruitment = isRecruitment;
         this.numOfAlpinists = numOfAlpinists;
-        this.alpinists = new ArrayList<>();
+        this.alpinists = new Alpinist[3];
         this.mountain = mountain;
     }
 
     public void addAlpinist(Alpinist alpinist) {
-        if (isRecruitment || (alpinists.size() < numOfAlpinists)) {
-            if (isRecruitment) isRecruitment = false;
-            alpinists.add(alpinist);
+        if (i != numOfAlpinists && isRecruitment) {
+            alpinists[i] = alpinist;
+            i++;
         } else {
             System.out.println("Набор в группу закрыт или группа полна.");
+        }
+    }
+    public void getAlpinist() {
+        int j = 0;
+        for (Alpinist alpinists : alpinists) {
+            if (j < i) {
+                System.out.printf("%s - %s - %s\n", alpinists.name, alpinists.age, alpinists.address);
+                j++;
+            } else break;
         }
     }
 }
